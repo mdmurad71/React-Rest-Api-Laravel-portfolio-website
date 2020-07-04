63404f07359fdd4e3db512d9672b62943f2869a2
Mohammad Murad <63662868+mdmurad71@users.noreply.github.com>
2020-07-04 11:53:14 +0600

    Update ContactPage.js

--- Admin/blog/resources/js/pages/ContactPage.js
+++ Admin/blog/resources/js/pages/ContactPage.js
@@ -4,6 +4,8 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 import Axios from "axios";
 import {Col, Container, Row} from "react-bootstrap";
+import LoadingDiv from "../components/loadingDiv";
+import WentWrong from "../components/wentWrong";
 
 class ContactPage extends Component {
 
@@ -12,55 +14,119 @@ class ContactPage extends Component {
         super();
         this.state={
             DataList:[],
+            isLoading:false,
+            isError:false,
+            rowDataId:"",
+            deleteBtnText:"Delete"
         }
+        this.dataDelete=this.dataDelete.bind(this);
     }
 
-    componentDidMount(axios = axios) {
+    componentDidMount() {
         Axios.get('/contactList').then((response)=>{
-            this.setState({DataList:response.data})
-            console.log(reponse)
+            if(response.status==200){
+                this.setState({DataList:response.data, isLoading:false, isError:false})
+            }else{
+                this.setState({isLoading:false, isError:true})
+            }
         }).catch((error)=>{
+            this.setState({isLoading:false, isError:true})
 
         })
 
     }
 
-
-
+dataDelete(){
+    let confirmResult= confirm("Do you want to Delete??")
+    if(confirmResult===true){
+        this.setState({deleteBtnText:"Deleting..."})
+    
+    Axios.post('/contactDelete', {id:this.state.rowDataId}).then((response)=>{
+        if(response.data==1 && response.status==200){
+            this.setState({deleteBtnText:"Delete Success"});
+            this.componentDidMount();
+            setTimeout(function(){
+                this.setState({deleteBtnText:"Delete"})
+            }.bind(this),2000)
+        }else{
+            this.setState({deleteBtnText:"Delete Fail"});
+            setTimeout(function(){
+                this.setState({deleteBtnText:"Delete"})
+            }.bind(this),2000)
+        }
+    }).catch((error)=>{
+        this.setState({deleteBtnText:"Delete Fail..."})
+        setTimeout(function () {
+            this.setState({deleteBtnText:"Delete"})
+        }.bind(this),2000)
+    })
+}
+}
 
 
 
     render() {
 
-        const data= this.state.DataList;
-
-
-        const column=[
-            {dataField:'id', text:'ID'},
-            {dataField:'contact_name', text:'Contact Name'},
-            {dataField:'contact_Mobile', text:'Contact Mobile'},
-            {dataField:'contact_email', text:'Email'},
-            {dataField:'contact_message', text:'Message'}
-
-        ]
-
-
-        return (
-            <Fragment>
-                <Menu>
+        if(this.state.isLoading==true){
+            return (
+                <Menu title="Contact">
                     <Container>
-                        <Row>
-                            <Col md={12} sm={12} lg={12}>
-                                <BootstrapTable keyField='id' data={ data } columns={ column } pagination={ paginationFactory() } />
-
-                            </Col>
-
-                        </Row>
+                        <LoadingDiv/>
                     </Container>
                 </Menu>
+            )
+        }
+        else if(this.state.isError==true){
+            return (
+                <Menu title="Contact">
+                    <Container>
+                        <WentWrong/>
+                    </Container>
+                </Menu>
+            )
+       }else{
+           const data= this.state.DataList;
+           const column=[
+               {dataField:'id', text:'ID'},
+               {dataField:'contact_name', text:'Contact Name'},
+               {dataField:'contact_Mobile', text:'Contact Mobile'},
+               {dataField:'contact_email', text:'Email'},
+               {dataField:'contact_message', text:'Message'}
 
-            </Fragment>
-        );
+           ]
+            const selectRow={
+                mode:'radio',
+                onSelect:(row, isSelect, rowIndex)=>{
+                    this.setState({rowDataId:row['id']})
+            }
+            }
+
+
+
+
+           return (
+               <Fragment>
+                   <Menu>
+                       <Container>
+                           <Row>
+                               <Col md={12} sm={12} lg={12}>
+           <button onClick={this.dataDelete} className="normal-btn my-2 btn">{this.state.deleteBtnText}</button>
+                                   <BootstrapTable
+                                       keyField='id'
+                                       data={ data }
+                                       columns={ column }
+                                       selectRow={selectRow}
+                                       pagination={ paginationFactory() } />
+
+                               </Col>
+
+                           </Row>
+                       </Container>
+                   </Menu>
+
+               </Fragment>
+           );
+       }
     }
 }
 
