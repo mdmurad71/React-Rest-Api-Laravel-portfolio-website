43cdfe35fba5d50b19dc09eea1f51da60d4d5fa1
Mohammad Murad <63662868+mdmurad71@users.noreply.github.com>
2020-07-09 17:54:50 +0600

    Update ReviewController.php

--- Admin/blog/app/Http/Controllers/ReviewController.php
+++ Admin/blog/app/Http/Controllers/ReviewController.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Model\ClientReviewModel;
 
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    //
+    function reviewList(){
+        $result=ClientReviewModel::all();
+        return $result;
+    }
+
+function reviewDelete(Request $request){
+   $id= $request->input('id');
+   $result=ClientReviewModel::where('id', '=', $id)->delete();
+   return $result;
+}
 }
