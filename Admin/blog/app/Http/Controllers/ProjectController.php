d744e24914e2242763d025160f3d2b6c091ce2d9
Mohammad Murad <63662868+mdmurad71@users.noreply.github.com>
2020-07-09 17:55:30 +0600

    Update ProjectController.php

--- Admin/blog/app/Http/Controllers/ProjectController.php
+++ Admin/blog/app/Http/Controllers/ProjectController.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Model\ProjectModel;
 
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    function projectList(){
+        $result=ProjectModel::all();
+        return $result;
+    }
+
+function projectDelete(Request $request){
+   $id= $request->input('id');
+   $result=ProjectModel::where('id', '=', $id)->delete();
+   return $result;
+}
 }
