aa40354ea40b9e7d821270e8f794a62120a547aa
Mohammad Murad <63662868+mdmurad71@users.noreply.github.com>
2020-07-09 17:57:10 +0600

    Update CourseController.php

--- Admin/blog/app/Http/Controllers/CourseController.php
+++ Admin/blog/app/Http/Controllers/CourseController.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Model\CourseTableModel;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
-    //
+    function courseList(){
+        $result=CourseTableModel::all();
+        return $result;
+    }
+
+function courseDelete(Request $request){
+   $id= $request->input('id');
+   $result=CourseTableModel::where('id', '=', $id)->delete();
+   return $result;
+}
 }
