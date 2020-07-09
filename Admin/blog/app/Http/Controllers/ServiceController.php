fd5847cae220a199135c83bb902e0a87777edba8
Mohammad Murad <63662868+mdmurad71@users.noreply.github.com>
2020-07-09 17:53:44 +0600

    Update ServiceController.php

--- Admin/blog/app/Http/Controllers/ServiceController.php
+++ Admin/blog/app/Http/Controllers/ServiceController.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Model\ServiceModel;
 
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    //
+    function serviceList(){
+        $result=ServiceModel::all();
+        return $result;
+    }
+
+function serviceDelete(Request $request){
+   $id= $request->input('id');
+   $result=ServiceModel::where('id', '=', $id)->delete();
+   return $result;
+}
 }
