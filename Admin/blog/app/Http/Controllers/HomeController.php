2e06a0ada0fac44fc3df266ec00fde550b89ae5b
Mohammad Murad <63662868+mdmurad71@users.noreply.github.com>
2020-07-09 17:56:21 +0600

    Update HomeController.php

--- Admin/blog/app/Http/Controllers/HomeController.php
+++ Admin/blog/app/Http/Controllers/HomeController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\ClientReviewModel;
+use App\Model\ContactTableModel;
+use App\Model\CourseTableModel;
+use App\Model\ProjectModel;
+use App\Model\ServiceModel;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    function CountSummary(){
+        $review=ClientReviewModel::count();
+        $contact=ContactTableModel::count();
+        $projects=ProjectModel::count();
+        $courses=CourseTableModel::count();
+        $service=ServiceModel::count();
+        $totalCount=array(
+            'review'=>$review, 
+            'contact'=>$contact, 
+            'projects'=>$projects,
+            'courses'=>$courses,
+            'service'=>$service,
+        );
+        return json_encode($totalCount);
+    }
 }
