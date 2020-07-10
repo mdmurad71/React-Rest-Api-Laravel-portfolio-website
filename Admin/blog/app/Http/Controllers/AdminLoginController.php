e6a6d6dd054a1b968b57ab08bd9617dffc0d629a
Mohammad Murad <63662868+mdmurad71@users.noreply.github.com>
2020-07-10 12:19:34 +0600

    Update AdminLoginController.php

--- Admin/blog/app/Http/Controllers/AdminLoginController.php
+++ Admin/blog/app/Http/Controllers/AdminLoginController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\AdminLoginModel;
 use Illuminate\Http\Request;
 
 class AdminLoginController extends Controller
 {
-    //
+    function LoginPage(){
+        return view('Login');
+    }
+
+    function onLogin(Request $request){
+        $UserName=$request->UserName;
+        $Password=$request->Password;
+        $Count=AdminLoginModel::where('user_name', $UserName)->where('password', $Password)->count();
+
+        if($Count==1){
+            $request->session()->put('userNameKey', $UserName);
+            return '1';
+        }else{
+            return '0';
+        }
+    }
+
+    function onLogout(Request $request){
+        $request->session()->flash();
+        return redirect('/LoginPage');
+    }
+
+
+
 }
